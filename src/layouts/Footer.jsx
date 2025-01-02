47cab1fa8d657d3ee06290b3bc82b3eb085c4080
Mohammadreza <ma3968312@gmail.com>
2025-01-02 23:03:27 +0330

    add layout to the project

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Developed by Mohammadreza ❤️</p>
+    </footer>
+  );
+}
+
+export default Footer;
